35da0ed0bafab541b7b1c5b4d8105d769777dc79
huuluan186 <luanphamhuu2004@gmail.com>
2025-10-25 15:47:11 +0700

    Config tailwind css for view ejs

---
+++ server/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./src/views/**/*.ejs", "./src/public/**/*.html"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
+
